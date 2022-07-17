4294b1680f4e5a6d71b675901224c2391dee5fb9
DESKTOP-9UJEHGC\maxi <nahuel_maxi11@hotmail.com>
2022-07-17 00:00:25 -0300

    Formulario Login

--- LPOO/Vistas/FormLogin.cs
+++ LPOO/Vistas/FormLogin.cs
@@ -9,11 +9,16 @@ using System.Windows.Forms;
 
 namespace Vistas
 {
-    public partial class Form1 : Form
+    public partial class FormLogin : Form
     {
-        public Form1()
+        public FormLogin()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
